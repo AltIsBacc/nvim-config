da123aa9445e037e8f12912f07c6e8ab5dfe863b
AltIsBacc <117834511+AltIsBacc@users.noreply.github.com>
2026-05-26 07:24:50 +0800

    fix(pyright): simplify pyright lsp config

--- lua/settings/lspservers/pyright.lua
+++ lua/settings/lspservers/pyright.lua
@@ -1,7 +1,5 @@
 return {
-	-- autostart = false,  -- still configured but vim.lsp.enable() won't start it
 	filetypes = { "python" },
-	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt" },
 	settings = {
 		["python"] = {
 			analysis = {
